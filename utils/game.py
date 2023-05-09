fd96e708cc7c9fa2ffaab4f5980ebcc8f881b30d
Maciej <maciej.krsk@gmail.com>
2023-05-09 10:14:52 +0200

    first commit from laptop

---
+++ utils/game.py
@@ -0,0 +1,40 @@
+class Hangman:
+
+    possible_words = ['becode', 'learning', 'mathematics', 'sessions']
+    #list of string
+    word_to_find = []
+    lives = 5
+
+    correctly_guessed_letters=[] 
+    wrongly_guessed_letters = []
+    turn_count = int
+    error_count = int
+
+    def __init__(self):
+        
+        pass
+        
+    def game_over():
+       return
+    
+    def well_played():
+        return
+    
+    def play():
+        return
+
+    def start_game():
+        play()
+
+        game_over()
+
+
+
+        return
+    
+    def game_over():
+       return
+    
+    def well_played():
+        return
+    
